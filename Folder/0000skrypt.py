be2d059b0f86f45d7ad8d3e1bdabf694d96eef06
machal3 <nowackimichalartur@gmail.com>
2026-01-23 23:18:27 +0100

    Dodano wszystko

--- Folder/0000skrypt.py
+++ Folder/0000skrypt.py
@@ -83,28 +83,30 @@ def format_song_to_file(text):
     print(f"Zapisano: {filename}")
 
 if __name__ == "__main__":
-    sample_text = """La Bamba
-Para bailar la Bamba	C F G
-Para bailar la Bamba
-Se necesita una poca de gracia
-Una poca de gracia
-Pa mi y pa ti
-Ahi arriba ahi arriba
-Ahi arriba ahi arriba
+    sample_text = """Ballada o spalonej synagodze
+Żydzi, Żydzi wstawajcie płonie synagoga!	a G a
+Cała Wschodnia Ściana porosła już żarem!	a G a
+Powietrze drży, skacze po suchych belkach ogień!	a G a
+Wstawajcie, chrońcie święte księgi wiary!	a G e a
 
-Por ti seré
-Por ti seré
-Yo no soy Marinero
-Yo no soy Marinero
-Soy Capitan soy Capitan
+Gromadzą się ciemni w krąg płonącego gmachu,	a G a
+Biegają iskry po pejsach, tlą się długie brody,	a G C
+W oczach blask pożaru, rozpaczy i strachu –	d C d a
+Gdzie będą teraz wznosić swoje próżne modły?!	a G e a
 
-Bamba Bamba Bamba Bamba
-Bamba Bamba Bamba
+Płonie synagoga! Trzask i krzyk gardłowy!	a G a
+Belki w żar się sypią, iskry w górę lecą!	a G a
+Tam, w środku Żyd został! Jeszcze widać ręce!	a G a
+Już czarne! O, Jehowo, za co? O, Jehowo?!	a G e a
 
-Para bailar la Bamba
-Para bailar la Bamba
-Se necesita una poca de gracia
-Una poca de gracia
-P mi y pa ti
-Ahi arriba ahi arriba"""
+Noc zapadła. Stoją wszyscy wielkim kołem,	a G a
+Chmury nisko, w ciemnościach gwiazda się dopala.	a G C
+Patrzą na swe chałaty porosłe popiołem,	d C d a
+Rząd rąk i twarzy w mrok się powoli oddala…	a G e a
+
+Stali tak do rana, deszcz spadł, ciągle stali	a d
+Aż popiół się zmieszał ze szlamem.	d a
+– Jeden Żyd się spalił! Jeden Żyd się spalił!	a
+Śpiewał w knajpie nad wódką włóczęga pijany.	d e a
+"""
     format_song_to_file(sample_text)
